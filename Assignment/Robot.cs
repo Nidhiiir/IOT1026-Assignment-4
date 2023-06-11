83a24a930e61baf5f5ee6fe4bee3c76b072dbd4c
Nidhiiir <131202460+Nidhiiir@users.noreply.github.com>
2023-06-11 19:52:13 -0400

    Update Robot.cs

--- Assignment/Robot.cs
+++ Assignment/Robot.cs
@@ -2,76 +2,49 @@
 //using Assignment.AbstractCommand;
 using Assignment.InterfaceCommand;
 
-namespace Assignment;
-
-public class Robot
+namespace Assignment
 {
-    // These are properties, you can replace these with traditional getters/setters if you prefer.
-    public int NumCommands { get; }
-    public int X { get; set; }
-    public int Y { get; set; }
-    public bool IsPowered { get; set; }
-
-    private const int DefaultCommands = 6;
-
-    // An array is not the preferred data structure here.
-    // You will get bonus marks if you replace the array with the preferred data structure
-    // Hint: It is NOT a list either,
-    private readonly RobotCommand[] _commands;
-    private int _commandsLoaded = 0;
-
-    public override string ToString()
+    public class Robot
     {
-        return $"[{X} {Y} {IsPowered}]";
-    }
+        // These are properties, you can replace these with traditional getters/setters if you prefer.
+        public int NumCommands { get; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsPowered { get; set; }
 
-    /// <summary>
-    /// Constructor named "Robot" with no parameters. It has an initializer that calls another constructor
-    /// </summary>
-    /// <constructor>
-    /// <modifier>public</modifier>
-    /// <name>Robot</name>
-    /// <parameters></parameters>
-    /// <initializer>this(DefaultCommands)</initializer>
-    /// <body>{ }</body>
-    /// </constructor>
-    public Robot() : this(DefaultCommands) { }
+        private const int DefaultCommands = 6;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="Robot"/> class with a specified number of commands.
-    /// </summary>
-    /// <param name="numCommands">The number of commands for the robot.</param>
-    public Robot(int numCommands)
-    {
-        _commands = new RobotCommand[numCommands];
-        NumCommands = numCommands;
-    }
+        // An array is not the preferred data structure here.
+        // You will get bonus marks if you replace the array with the preferred data structure
+        // Hint: It is NOT a list either,
+        private readonly IRobotCommand[] _commands;
+        private int _commandsLoaded = 0;
 
-    /// <summary>
-    /// Executes the loaded commands for the robot and prints the robot's state after each command execution.
-    /// </summary>
-    public void Run()
-    {
-        for (var i = 0; i < _commandsLoaded; ++i)
+        public override string ToString()
         {
-            _commands[i].Run(this);
-            Console.WriteLine(this);
+            return $"[{X} {Y} {IsPowered}]";
         }
-    }
 
-    /// <summary>
-    /// Loads a command into the robot's command array.
-    /// </summary>
-    /// <param name="command">The command to be loaded.</param>
-    /// <returns>
-    /// <c>true</c> if the command was successfully loaded;
-    /// otherwise, <c>false</c> if the command list is already full.
-    /// </returns>
-    public bool LoadCommand(RobotCommand command)
-    {
-        if (_commandsLoaded >= NumCommands)
-            return false;
-        _commands[_commandsLoaded++] = command;
-        return true;
+        /// <summary>
+        /// Constructor named "Robot" with no parameters. It has an initializer that calls another constructor
+        /// </summary>
+        /// <constructor>
+        /// <modifier>public</modifier>
+        /// <name>Robot</name>
+        /// <parameters></parameters>
+        /// <initializer>this(DefaultCommands)</initializer>
+        /// <body>{ }</body>
+        /// </constructor>
+        public Robot() : this(DefaultCommands) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Robot"/> class with a specified number of commands.
+        /// </summary>
+        /// <param name="numCommands">The number of commands for the robot.</param>
+        public Robot(int numCommands)
+        {
+            _commands = new IRobotCommand[numCommands];
+            NumCommands = numCommands;
+        }
     }
 }
