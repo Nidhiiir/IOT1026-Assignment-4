a957ebf8f2286bba8b19ab9734acf43b9aee367d
Nidhiiir <131202460+Nidhiiir@users.noreply.github.com>
2023-06-11 19:53:55 -0400

    Update AssignmentTest.cs

--- AssignmentTest/AssignmentTest.cs
+++ AssignmentTest/AssignmentTest.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment.InterfaceCommand;
+
 namespace AssignmentTest
 {
     [TestClass]
@@ -102,62 +105,5 @@ namespace AssignmentTest
         public void PropertiesTest()
         {
             Robot robot1 = new();
-            Assert.AreEqual(robot1.NumCommands, 6);
-            const int ExpectedCommands = 10;
-            Robot robot2 = new(ExpectedCommands);
-            Assert.AreEqual(robot2.NumCommands, ExpectedCommands);
+            Assert.AreEqual(robot1.NumCommands
 
-            Assert.AreEqual(robot1.IsPowered, false);
-            robot1.IsPowered = true;
-            Assert.AreEqual(robot1.IsPowered, true);
-
-            Assert.AreEqual(robot1.X, 0);
-            // Moves the robot can move even though it is off!!
-            // This is very bad! Not good encapsulation
-            robot1.X = -5;
-            Assert.AreEqual(robot1.X, -5);
-
-            Assert.AreEqual(robot1.Y, 0);
-            robot1.Y = -5;
-            Assert.AreEqual(robot1.Y, -5);
-        }
-
-        [TestMethod]
-        public void OnCommandTest()
-        {
-            Robot testrobot = new();
-            Assert.AreEqual(testrobot.IsPowered, false);
-            testrobot.LoadCommand(new OnCommand());
-            testrobot.Run();
-            Assert.AreEqual(testrobot.IsPowered, true);
-            testrobot.Run();
-            Assert.AreEqual(testrobot.IsPowered, true);
-        }
-
-        [TestMethod]
-        public void OffCommandTest()
-        {
-            Robot testRobot = new();
-            Assert.AreEqual(testRobot.IsPowered, false);
-            testRobot.IsPowered = true;
-            testRobot.LoadCommand(new OffCommand());
-            testRobot.Run();
-            Assert.AreEqual(testRobot.IsPowered, false);
-            testRobot.Run();
-            Assert.AreEqual(testRobot.IsPowered, false);
-        }
-        [TestMethod]
-        public void TestAssignCommand()
-        {
-            Robot robot = new();
-            string[] validCommands = { "on", "off", "north", "south", "east", "west" };
-
-            bool result = true;
-            foreach (string command in validCommands)
-            {
-                result &= Program.AssignCommand(robot, command);
-            }
-            Assert.AreEqual(result, true);
-        }
-    }
-}
