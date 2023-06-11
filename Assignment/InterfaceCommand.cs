24e97f4ad7710302cc7c0a2e1fcaf9dce63b3a27
Nidhiiir <131202460+Nidhiiir@users.noreply.github.com>
2023-06-11 19:14:23 -0400

    Update InterfaceCommand.cs

--- Assignment/InterfaceCommand.cs
+++ Assignment/InterfaceCommand.cs
@@ -1,6 +1,58 @@
-﻿namespace Assignment.InterfaceCommand;
+namespace Assignment.InterfaceCommand;
 
-public interface IRobotCommand
+public interface RobotCommand
 {
     void Run(Robot robot); // Interfaces are public and abstract by default.
+}
+
+public class OffCommand : RobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = false;
+}
+
+public class OnCommand : RobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = true;
+}
+
+public class WestCommand : RobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.X--; }
+}
+
+public class EastCommand : RobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.X++; }
+}
+
+public class SouthCommand : RobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.Y--; }
+}
+
+public class NorthCommand : RobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.Y++; }
+}
+
+public class BachataCommand : RobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            for (int repeatSteps = 0; repeatSteps < 3; repeatSteps++)
+            {
+                robot.X++;
+                robot.X++;
+                robot.X--;
+                robot.X--;
+                robot.X--;
+            }
+            robot.Y++;
+            robot.X--;
+            robot.Y--;
+            robot.X++;
+        }
+    }
 }
