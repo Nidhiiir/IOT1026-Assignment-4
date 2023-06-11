11b60c4b7e9e148204b695b5d4862eaba7ddbb0c
Nidhiiir <131202460+Nidhiiir@users.noreply.github.com>
2023-06-11 19:13:52 -0400

    Update Program.cs

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,13 +1,74 @@
-﻿namespace Assignment
+using Assignment.InterfaceCommand;
+
+namespace Assignment
 {
-    static class Program
+    public static class Program
     {
+        enum Commands
+        {
+            on,
+            off,
+            north,
+            south,
+            east,
+            west
+        }
+        public static bool AssignCommand(Robot robot, string? command)
+        {
+            if (Enum.TryParse<Commands>(command, out Commands thisCommand))
+            {
+                switch (thisCommand)
+                {
+                    case Commands.on:
+                        robot.LoadCommand(new OnCommand());
+                        break;
+                    case Commands.off:
+                        robot.LoadCommand(new OffCommand());
+                        break;
+                    case Commands.north:
+                        robot.LoadCommand(new NorthCommand());
+                        break;
+                    case Commands.south:
+                        robot.LoadCommand(new SouthCommand());
+                        break;
+                    case Commands.east:
+                        robot.LoadCommand(new EastCommand());
+                        break;
+                    case Commands.west:
+                        robot.LoadCommand(new WestCommand());
+                        break;
+                }
+                return true;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid command - Try again");
+                Console.ResetColor();
+                return false;
+            }
+        }
         static void Main()
         {
-            RobotTester tester = new RobotTester();
-            tester.TestRobot();
+            // Run your RobotTester class here -> RobotTester.TestRobot()
+            Robot newRobot = new();
+            Console.WriteLine("Give 6 commands to the robot. Possible commands are:");
+            Commands[] allCommands = (Commands[])Enum.GetValues(typeof(Commands));
 
-            Console.ReadLine();
+            foreach (Commands command in allCommands)
+            {
+                Console.WriteLine(command);
+            }
+            for (int input = 0; input < 6; input++)
+            {
+                Console.Write($"Assign command #{input + 1}: ");
+                bool commandAdded = AssignCommand(newRobot, Console.ReadLine());
+                if (!commandAdded)
+                {
+                    input--;
+                }
+            }
+            newRobot.Run();
         }
     }
 }
