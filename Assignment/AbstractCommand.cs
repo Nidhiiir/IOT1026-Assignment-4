45fed1f089139e0edf572a8d8388f61124737654
Nidhiiir <131202460+Nidhiiir@users.noreply.github.com>
2023-06-11 19:58:45 -0400

    Update AbstractCommand.cs

--- Assignment/AbstractCommand.cs
+++ Assignment/AbstractCommand.cs
@@ -1,67 +1,67 @@
 namespace Assignment.AbstractCommand
 {
-public abstract class RobotCommand
-{
-public abstract void Run(Robot robot);
-}
-public class OffCommand : RobotCommand
-{
-    public override void Run(Robot robot)
+    public abstract class RobotCommand
     {
-        robot.IsPowered = false;
+        public abstract void Run(Robot robot);
     }
-}
 
-public class OnCommand : RobotCommand
-{
-    public override void Run(Robot robot)
+    public class OffCommand : RobotCommand
     {
-        robot.IsPowered = true;
-    }
-}
-
-public class WestCommand : RobotCommand
-{
-    public override void Run(Robot robot)
-    {
-        if (robot.IsPowered)
+        public override void Run(Robot robot)
         {
-            robot.X--;
+            robot.IsPowered = false;
+        }
+    }
+
+    public class OnCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            robot.IsPowered = true;
+        }
+    }
+
+    public class WestCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered)
+            {
+                robot.X--;
+            }
+        }
+    }
+
+    public class EastCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered)
+            {
+                robot.X++;
+            }
+        }
+    }
+
+    public class SouthCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered)
+            {
+                robot.Y--;
+            }
+        }
+    }
+
+    public class NorthCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered)
+            {
+                robot.Y++;
+            }
         }
     }
 }
-
-public class EastCommand : RobotCommand
-{
-    public override void Run(Robot robot)
-    {
-        if (robot.IsPowered)
-        {
-            robot.X++;
-        }
-    }
-}
-
-public class SouthCommand : RobotCommand
-{
-    public override void Run(Robot robot)
-    {
-        if (robot.IsPowered)
-        {
-            robot.Y--;
-        }
-    }
-}
-
-public class NorthCommand : RobotCommand
-{
-    public override void Run(Robot robot)
-    {
-        if (robot.IsPowered)
-        {
-            robot.Y++;
-        }
-    }
-}
-
-    
